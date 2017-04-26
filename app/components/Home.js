f2a57d35c1cea8ec238afa2a61dc7889bb1a4155
Rob Lingle <rob@oak.works>
2017-04-26 11:38:58 -0400

    Rename TimerContainer to TimerPanel

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,14 +2,14 @@
 import React, { Component } from 'react';
 import styles from './Home.css';
 
-import TimerContainer from '../containers/TimerContainer';
+import TimerPanel from '../containers/TimerPanel';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <div className={styles.container} data-tid="container">
-          <TimerContainer />
+          <TimerPanel />
         </div>
       </div>
     );
