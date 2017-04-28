0d27ab1d6f0b0e5b085fb086543a4a81915fca1d
Daniel Swain <danieswain@gmail.com>
2017-04-28 11:29:12 -0400

    RfidRelay: ↗️ RSServer connection retries on failure

--- app/server/index.js
+++ app/server/index.js
@@ -2,6 +2,7 @@ import net from 'net';
 import moment from 'moment';
 import { addMessage } from '../actions/status';
 
+const RSSERVER_CONNECTION_ATTEMPTS = 5;
 export default class RfidRelay {
   store;
   rfidListener: net.Server;
@@ -17,7 +18,7 @@ export default class RfidRelay {
   }
 
   stop() {
-    console.log('Stopping');
+    this.store.dispatch(addMessage('Stopping...'));
     this.rfidListener.close();
     this.runScoreConn.destroy();
   }
@@ -45,20 +46,44 @@ export default class RfidRelay {
       port: runScorePort
     };
 
-    const attemptConnection = (conn) => {
-      this.store.dispatch(addMessage('Connected to RSServer'));
-      this.runScoreConn = conn;
-    };
-
-    this.runScore = net.connect(serverInfo, attemptConnection);
-
-    this.runScore.on('error', (error) => {
-      this.store.dispatch(addMessage(`Unable to connect to RSServer: ${error.message}`));
-    });
+    this.attemptRSServerConnection(serverInfo, RSSERVER_CONNECTION_ATTEMPTS);
 
     this.runScore.on('close', () => {
       this.store.dispatch(addMessage('Disconnected from RSServer'));
       this.runScoreConn = null;
+    });
+  }
+
+  /**
+   * attemptRSServerConnection
+   *
+   * Recursive function to attempt connection to the RSServer.
+   * This function will only call recursively if it fails.
+   *
+   * @param {Server}          - Address and Port information
+   * @param {Number} attempts - Number of attempts left to retry connection
+   * @memberOf RfidRelay
+   */
+  attemptRSServerConnection = (serverInfo: { host: string, port: number }, attempts: number) => {
+    if (attempts === 0) {
+      this.store.dispatch(addMessage(`
+        Please review server setup.
+      `));
+      return;
+    }
+
+    this.store.dispatch(addMessage('Attempting connection to RSServer...'));
+    this.runScore = net.connect(serverInfo, (conn) => {
+      this.store.dispatch(addMessage('Connected to RSServer!'));
+      this.runScoreConn = conn;
+    });
+
+    this.runScore.on('error', (error) => {
+      this.store.dispatch(addMessage(`
+        Failed to connect to RSServer: ${error.message}
+        (${attempts}) attempts left
+      `));
+      this.attemptRSServerConnection(serverInfo, attempts - 1);
     });
   }
 
