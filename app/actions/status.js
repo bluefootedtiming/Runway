969c548b9c3583224a95ee35f8c3cd6256e2c088
Daniel Swain <danieswain@gmail.com>
2017-04-28 14:52:12 -0400

    status/actions: ↗️ Added formatting for better grep & timestamp

--- app/actions/status.js
+++ app/actions/status.js
@@ -1,8 +1,14 @@
+import moment from 'moment';
+
 export const ADD_MESSAGE = 'ADD_MESSAGE';
 
-export function addMessage(message: string) {
+const MESSAGE_TYPES = [
+  'INFO', 'ERROR'
+];
+
+export function addMessage(message: string, code: number = 0) {
   return {
     type: ADD_MESSAGE,
-    payload: message
+    payload: `[${MESSAGE_TYPES[code]}] ${moment().format('hh:mm:ss')}> ${message}`
   };
 }
