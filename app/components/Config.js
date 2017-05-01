799fdf5d157d19f8f1c207fa0da8d2f9bd07f4dd
Rob Lingle <rob@oak.works>
2017-05-01 14:15:17 -0400

    ↗️ Allow config save for RunScore address/port and listener

--- app/components/Config.js
+++ app/components/Config.js
@@ -12,25 +12,20 @@ class Configuration extends Component {
     readerMap: readerMapType
   }
 
-  // XXX: Replace with onSave
-  handleChange = e => {
-    const { name, value } = e.currentTarget.name;
-    switch (name) {
-      case 'runScoreAddress':
-        return this.props.setRunScoreAddress(value);
-
-      case 'runScorePort':
-        return this.props.setRunScorePort(value);
-
-      case 'listenPort':
-        return this.props.setListenPort(value);
-
-      default:
-    }
-  }
-
   addReader = () => {
 
+  }
+
+  onSave = () => {
+    const {
+      runScoreAddress: { value: runScoreAddress },
+      runScorePort: { value: runScorePort },
+      listenPort: { value: listenPort }
+    } = this;
+
+    if (runScoreAddress) { this.props.setRunScoreAddress(runScoreAddress); }
+    if (runScorePort) { this.props.setRunScorePort(Number(runScorePort)); }
+    if (listenPort) { this.props.setListenPort(Number(listenPort)); }
   }
 
   render() {
@@ -47,23 +42,36 @@ class Configuration extends Component {
         <h2>RFID Listen Port</h2>
         <aside>
           RFID Readers should use the IP address of this
-           computer and the port listed below in as <b>TagStreamAddress</b>
+          computer and the port listed below in as <b>TagStreamAddress</b>
         </aside>
         <input placeholder="Listen Port" name="listenPort" defaultValue={listenPort} ref={c => (this.listenPort = c)} />
-
-        <h2>RFID Locations <i className="fa fa-plus-circle" onClick={this.addReader} role="button" /></h2>
+        {/*
+        <h2>
+          RFID Locations
+          <i className="fa fa-plus-circle" onClick={this.addReader} role="button" />
+        </h2>
         <aside>
           Configure Readers to use TagStreamFormat <b>%i,%N,%T</b>.
-           The section below is only needed if you would rather not set
-           <b>ReaderName</b> on your readers to match RunScore events.
+          The section below is only needed if you would rather not set
+          <b>ReaderName</b> on your readers to match RunScore events.
         </aside>
         {Object.keys(readerMap).map(address => (
           <div key={`reader-${address}`}>
-            <input placeholder="IP Address" defaultValue={address} ref={c => (this[`address-${address}`] = c)} />
-            <input placeholder="Location/Event" defaultValue={readerMap[address]} ref={c => (this[`name-${address}`] = c)} />
+            <input
+              placeholder="IP Address"
+              defaultValue={address}
+              ref={c => (this[`address-${address}`] = c)}
+            />
+            <input
+              placeholder="Location/Event"
+              defaultValue={readerMap[address]}
+              ref={c => (this[`name-${address}`] = c)}
+            />
             <i className="fa fa-minus-circle" />
           </div>
         ))}
+        */}
+        <br />
         <button onClick={this.onSave}>Save Settings</button>
       </section>
     );
