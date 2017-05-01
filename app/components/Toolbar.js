6ad6e3c3ebe90bee1212fb99e4af3eab12603b24
Daniel Swain <danieswain@gmail.com>
2017-05-01 13:20:24 -0400

    toolbar: 🎉 Server Status buttton with retry function

--- app/components/Toolbar.js
+++ app/components/Toolbar.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Status from '../containers/AppStatus';
 import Config from '../containers/AppConfig';
 import styles from './toolbar.scss';
+import { relay } from '../index';
 
 const { BrowserWindow } = require('electron').remote;
 
@@ -10,13 +11,15 @@ const { BrowserWindow } = require('electron').remote;
 
 export default class Tools extends Component {
   state: {
-    currentTool?: string
+    currentTool?: string,
+      isConnectedToRSServer: boolean
   };
 
   constructor() {
     super();
     this.state = {
-      currentTool: null
+      currentTool: null,
+      isConnectedToRSServer: false
     };
   }
 
@@ -32,26 +35,46 @@ export default class Tools extends Component {
     this.setState({ currentTool });
   }
 
+  retryServerConnection = () => {
+    if (!relay.isConnectedToRSServer) {
+      relay.connectToRSServer();
+    }
+    this.setState({ isConnectedToRSServer: relay.connected });
+  }
+
   render() {
-    const { currentTool } = this.state;
+    const { currentTool, isConnectedToRSServer } = this.state;
 
     return (
-      <div className={styles.container} >
-        <div className={styles.buttons}>
-          <button name="status" onClick={this.toggleTool}>
-            <i className="fa fa-align-justify" />
-          </button>
-          <button name="config" onClick={this.toggleTool}>
-            <i className="fa fa-wrench" />
-          </button>
-        </div>
-
-        { currentTool && (
-          <div className="tool">
-            { currentTool === 'status' && <Status />}
-            { currentTool === 'config' && <Config />}
+      <div>
+        <div className={styles.toolbar_button_container} >
+          <div className={styles.left_buttons}>
+            <button
+              name="server"
+              className={isConnectedToRSServer ? styles.connected : styles.disconnected}
+              title={`${isConnectedToRSServer ? 'Connected' : 'Not connected'} to RunScore Server`}
+              onClick={!isConnectedToRSServer && this.retryServerConnection}
+            >
+              <i className="fa fa-flash" />
+            </button>
           </div>
-        )}
+          <div>
+            <button name="status" onClick={this.toggleTool}>
+              <i className="fa fa-align-justify" />
+            </button>
+            <button name="config" onClick={this.toggleTool}>
+              <i className="fa fa-wrench" />
+            </button>
+          </div>
+        </div>
+        <div className={styles.toolbar_panel_container}>
+          { currentTool && (
+            <div className="tool">
+              { currentTool === 'status' && <Status />}
+              { currentTool === 'config' && <Config />}
+            </div>
+          )}
+        </div>
       </div>
     );
   }
