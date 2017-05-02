2003778d1dc425c7d78b67536d991c2ec9060ff5
Daniel Swain <danieswain@gmail.com>
2017-05-02 18:07:26 -0400

    config: ↗️ Added back readerMap configuration

--- app/components/Config.js
+++ app/components/Config.js
@@ -6,30 +6,120 @@ class Configuration extends Component {
     setRunScoreAddress: () => void,
     setRunScorePort: () => void,
     setListenPort: () => void,
+    addReader: () => void,
+    delReader: () => void,
     runScoreAddress: string,
     runScorePort: number,
     listenPort: number,
     readerMap: readerMapType
   }
 
-  addReader = () => {
+  state: {
+    readerAddresses: Array<string>
+  }
 
+  static defaultProps = {
+    readerAddresses: []
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      readerAddresses: []
+    };
+  }
+
+  componentWillMount() {
+    const vals = Object.keys(this.props.readerMap).length ? (
+      Object.keys(this.props.readerMap)
+    ) : (
+      ['']
+    );
+    this.setState({ readerAddresses: vals });
+  }
+
+  addReader = () => {
+    const { readerAddresses } = this.state;
+    if (!readerAddresses.includes('')) {
+      this.setState({
+        readerAddresses: readerAddresses.concat([''])
+      });
+    }
+  }
+
+  removeReader = (e) => {
+    const key = e.currentTarget.name.split('remove-')[1];
+    if (this.props.readerMap[key]) this.props.delReader(key);
+
+    const newAddresses = this.state.readerAddresses;
+    const index = key === 'new' ? (
+      this.state.readerAddresses.indexOf('')
+    ) : (
+      this.state.readerAddresses.indexOf(key)
+    );
+    newAddresses.splice(index, 1);
+    this.setState({
+      readerAddresses: newAddresses
+    });
   }
 
   onSave = () => {
     const {
       runScoreAddress: { value: runScoreAddress },
       runScorePort: { value: runScorePort },
-      listenPort: { value: listenPort }
+      listenPort: { value: listenPort },
+      props: { readerMap }
     } = this;
 
     if (runScoreAddress) { this.props.setRunScoreAddress(runScoreAddress); }
     if (runScorePort) { this.props.setRunScorePort(Number(runScorePort)); }
     if (listenPort) { this.props.setListenPort(Number(listenPort)); }
+
+    this.state.readerAddresses.forEach(key => {
+      if (!this[`address-${key}`] || !this[`name-${key}`]) {
+        this.props.addReader({
+          address: this['address-new'].value,
+          name: this['name-new'].value
+        });
+      } else if (this[`address-${key}`].value.address !== key
+        || this[`name-${key}`].value.name !== readerMap[key]) {
+        this.props.delReader(key);
+        this.props.addReader({
+          address: this[`address-${key}`].value,
+          name: this[`name-${key}`].value
+        });
+      }
+    });
+    this.setState({ readerAddresses: Object.keys(readerMap) });
+  }
+
+  readerMapInputFields = (address) => {
+    const key = address || 'new';
+    return (
+      <div key={`reader-${key}`}>
+        <input
+          placeholder="IP Address"
+          defaultValue={address}
+          ref={c => (this[`address-${key}`] = c)}
+        />
+        <input
+          placeholder="Location/Event"
+          defaultValue={this.props.readerMap[address]}
+          ref={c => (this[`name-${key}`] = c)}
+        />
+        <button
+          name={`remove-${key}`}
+          style={{ backgroundColor: 'white', margin: 0 }}
+          onClick={this.removeReader}
+        >
+          <i className="fa fa-minus-circle" />
+        </button>
+      </div>
+    );
   }
 
   render() {
-    const { runScoreAddress, runScorePort, listenPort, readerMap } = this.props;
+    const { runScoreAddress, runScorePort, listenPort } = this.props;
 
     return (
       <section>
@@ -45,7 +135,6 @@ class Configuration extends Component {
           computer and the port listed below in as <b>TagStreamAddress</b>
         </aside>
         <input placeholder="Listen Port" name="listenPort" defaultValue={listenPort} ref={c => (this.listenPort = c)} />
-        {/*
         <h2>
           RFID Locations
           <i className="fa fa-plus-circle" onClick={this.addReader} role="button" />
@@ -55,22 +144,7 @@ class Configuration extends Component {
           The section below is only needed if you would rather not set
           <b>ReaderName</b> on your readers to match RunScore events.
         </aside>
-        {Object.keys(readerMap).map(address => (
-          <div key={`reader-${address}`}>
-            <input
-              placeholder="IP Address"
-              defaultValue={address}
-              ref={c => (this[`address-${address}`] = c)}
-            />
-            <input
-              placeholder="Location/Event"
-              defaultValue={readerMap[address]}
-              ref={c => (this[`name-${address}`] = c)}
-            />
-            <i className="fa fa-minus-circle" />
-          </div>
-        ))}
-        */}
+        {this.state.readerAddresses.map(address => this.readerMapInputFields(address))}
         <br />
         <button onClick={this.onSave}>Save Settings</button>
       </section>
