dae621679a6b73bd7bda6cd618792f40b7fc47a8
Daniel Swain <danieswain@gmail.com>
2017-05-06 12:49:59 -0400

    dropdownselect: 🎉 Created select component

---
+++ app/components/DropdownSelect.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const DropdownSelect = (props) => (
+  <select
+    defaultValue={props.defaultValue}
+    onChange={props.onChange}
+  >
+    {props.options.length === 0 || !props.defaultValue ? (
+      <option>{props.placeHolder}</option>
+    ) : (
+      props.options.map(value => (
+        <option key={value} label={value} value={value} />
+    )))}
+  </select>
+);
+
+DropdownSelect.defaultProps = {
+  placeHolder: '',
+  onChange: null
+};
+
+DropdownSelect.propTypes = {
+  defaultValue: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  placeHolder: PropTypes.string,
+  onChange: PropTypes.func
+};
+
+export default DropdownSelect;
