72e56c8ed473481564cfe54c26988fb9d29af297
Daniel Swain <danieswain@gmail.com>
2017-05-22 09:47:27 -0400

    syncReaders: 🔄 Removed LLRP functions
    These functions will be handled in the server file

--- app/components/SyncReaders/index.js
+++ app/components/SyncReaders/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import net from 'net';
 import telnet from 'telnet-client';
-import { Buffer } from 'buffer';
 
 import { notify } from '../Config';
 import Button from '../Button';
-
 /**
   * SyncReaders
   *
@@ -24,50 +20,22 @@ const SyncReaders = ({ listenAddress, listenPort, readerMap, addMessage }) => {
     *
     * @param {string} event
     */
-  const readerConfigs = (event: string = '') => ({
-    LLRP: {
-      bSetReaderConfig: Buffer.from('040300000010000000000000e2000580', 'hex'),
-      bEnableEventsAndReport: Buffer.from('04400000000a00000000', 'hex'),
-      bAddRoSpec: Buffer.from('04140000005d0000000000b1005300000001000000b2001200b300050000b60009000000000000b700180001000000b8000901000003e800ba000700010100ed001f01000000ee000bffc0015c0005c003ff000d000067ba0000008e01', 'hex'),
-      bEnableRoSpec: Buffer.from('04180000000e0000000000000001', 'hex'),
-      bStartRoSpec: Buffer.from('04160000000e0000000000000001', 'hex'),
-      bKeepaliveAck: Buffer.from('04480000000a00000000', 'hex'),
-    },
-    NON_LLRP: {
-      username: 'alien',
-      password: 'password',
-      ReaderName: event,
-      AutoMode: 'On',
-      // AcquireMode: 'Inventory',
-      // AcqG2Mask: '',
-      NotifyMode: 'Off',
-      StreamHeader: 'Off',
-      TagStreamMode: 'On',
-      TagStreamFormat: 'Custom',
-      TagStreamAddress: `${listenAddress}:${listenPort}`,
-      TagStreamCustomFormat: 'RSBI,%i,%T,%N',
-    }
+  const readerConfigs = (event: string) => ({
+    username: 'alien',
+    password: 'password',
+    ReaderName: event,
+    AutoMode: 'On',
+    // AcquireMode: 'Inventory',
+    // AcqG2Mask: '',
+    NotifyMode: 'Off',
+    StreamHeader: 'Off',
+    TagStreamMode: 'On',
+    TagStreamFormat: 'Custom',
+    TagStreamAddress: `${listenAddress}:${listenPort}`,
+    TagStreamCustomFormat: 'RSBI,%i,%T,%N',
   });
 
   let error = false;
-
-  const LLRPSetup = (reader, configs) => {
-    console.log(`Connecting to: ${reader.address}:${reader.port}`);
-    const conn = net.Socket();
-    conn.connect({
-      host: reader.address,
-      port: reader.port
-    }, () => {
-      console.log(`Connected to: ${reader.address}:${reader.port}`);
-      Object.entries(configs).forEach(([, buffer]) => conn.write(buffer));
-    });
-
-    conn.on('data', data => {
-      console.log('Recieved:', data);
-    });
-
-    conn.on('error', () => { error = true; });
-  };
 
   /**
     * nonLLRPSetup
@@ -113,10 +81,8 @@ const SyncReaders = ({ listenAddress, listenPort, readerMap, addMessage }) => {
 
   const sync = () => {
     readerMap.forEach(reader => {
-      if (reader.isLLRP) {
-        LLRPSetup(reader, readerConfigs().LLRP);
-      } else {
-        nonLLRPSetup(reader, readerConfigs(reader.event).NON_LLRP);
+      if (!reader.isLLRP) {
+        nonLLRPSetup(reader, readerConfigs(reader.event));
       }
     });
     if (!error) notify('Reader sync complete');
